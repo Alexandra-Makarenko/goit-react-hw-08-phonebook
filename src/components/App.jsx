b17c2e8a5139d3999a34149dcf61698079289a87
Alexandra-Makarenko <s850272@gmail.com>
2022-11-17 19:50:05 +0200

    react

---
+++ src/components/App.jsx
@@ -0,0 +1,34 @@
+
+import {ContactForm} from './ContactForm/ContactForm'
+import { Filter } from './Filter/Filter'
+import {Layout} from './Layout/Layout'
+import { ContactList } from './ContactList/ContactList'
+import { useDispatch, useSelector } from "react-redux";
+import { useEffect } from "react";
+import { fetchContacts } from "redux/operations";
+import { selectError, selectIsLoading } from "redux/selectors";
+
+
+export const App = () => {
+  const dispatch = useDispatch();
+  const isLoading = useSelector(selectIsLoading);
+  const error = useSelector(selectError);
+
+  useEffect(() => {
+    dispatch(fetchContacts());
+  }, [dispatch]);
+
+  return (
+     <Layout>
+      <h1>Phonebook</h1>
+              <ContactForm />
+              <h2>Contacts</h2>
+              <Filter />
+      {isLoading && !error && <b>Request in progress...</b>}
+      <ContactList />
+    </Layout>
+  );
+};
+
+
+
