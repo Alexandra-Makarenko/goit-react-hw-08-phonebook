e1dcb6731d148fa37de444b151f5ab01b11053fe
Alexandra-Makarenko <37274881+Alexandra-Makarenko@users.noreply.github.com>
2022-11-24 14:55:37 +0200

    basename

--- src/index.js
+++ src/index.js
@@ -18,7 +18,7 @@ const theme = extendTheme({ colors })
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Provider store={store}>
-      <BrowserRouter>
+      <BrowserRouter basename="/goit-react-hw-05-movies-1/">
         <ChakraProvider theme={theme}>
           <App />
           </ChakraProvider>
